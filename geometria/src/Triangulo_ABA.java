2439448abd861f20282c5dccf1afad15ef8dbd1d
Adrian Bravo <bravoacm96@gmail.com>
2021-02-21 15:10:40 +0100

    metodo en una linea

--- geometria/src/Triangulo_ABA.java
+++ geometria/src/Triangulo_ABA.java
@@ -19,11 +19,7 @@ public class Triangulo_ABA extends FiguraGeometrica_ABA {
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
-		sp = this.semiPerimetro();
+		sp = this.perimetro() / 2;
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
-	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
 	}
 }
