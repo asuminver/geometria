729d1b42482c198e74882a8612e1256eb27a80b8
PMC07 <andvic.1eg1314@gmail.com>
2022-02-12 21:48:26 +0100

    Nuevo método

--- geometria/src/Triangulo_APV.java
+++ geometria/src/Triangulo_APV.java
@@ -23,7 +23,5 @@ public class Triangulo_APV extends FiguraGeometrica_APV {
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+	
 }
