e7b90ac6be0e2ca7cda0eaf64118dfcae2484cf4
jorgealll <117639889+jorgealll@users.noreply.github.com>
2023-03-07 17:54:50 +0100

    Update Rectangulo.java

--- geometria/src/Rectangulo.java
+++ geometria/src/Rectangulo.java
@@ -1,6 +1,7 @@
-/**@author Jorge Altet Zaragoza
-@version 1.2
-**/
+/**
+*@author Jorge Altet Zaragoza
+*@version 1.2
+*/
 public class Rectangulo_JAZ extends FiguraGeometrica_JAZ {
 	private double l1;
 	private double l2;
@@ -12,13 +13,21 @@ public class Rectangulo_JAZ extends FiguraGeometrica_JAZ {
 	}
 
 	@Override
-	public double area() { /*este metodo calcula el area*/
-		return l1 * l2;/*devuelve el resultado de l1 * l2*/
+	public double area() { /**
+	*este metodo calcula el area
+	*/
+		return l1 * l2;/**
+		*devuelve el resultado de l1 * l2
+		*/
 	}
 	
 	@Override
-	public double perimetro() {/*este metodo calcula el perimetro*/
-		return (2 * l1) + (2 * l2);/*devuelve el resultado de 2 * l1 + 2*l2*/
+	public double perimetro() {/**
+	*este metodo calcula el perimetro
+	*/
+		return (2 * l1) + (2 * l2);/**
+		*devuelve el resultado de 2 * l1 + 2*l2
+		*/
 	}
 
 }
